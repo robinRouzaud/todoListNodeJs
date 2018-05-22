816f38ab330ad83e42de197ae00c011b410f1622
Robin ROUZAUD <robinrouzaud@macbook-pro-de-robin-1.home>
2018-05-22 23:05:00 +0200

    Ajout des modules 'cookie-session' et 'body-parser'

--- todoList.js
+++ todoList.js
@@ -2,6 +2,8 @@ var http = require('http');
 var express = require('express');
 var url = require('url');
 var querystring = require('querystring');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -12,7 +14,10 @@ app.get('/', function(req, res) {
     res.send("On va essayer de faire une ToDo list dans cet exercice");
 })
 .get('/todolist', function(req, res) {
-    res.render('todoListPage.ejs', {pasEncoreUneListe: 9});
+    res.render('todoListPage.ejs');
+})
+.post('/todolist/ajouter', function(req, res) {
+
 })
 .use(function(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
@@ -20,14 +25,11 @@ app.get('/', function(req, res) {
 });
 
 /*Implémentation de la ToDo list:
-Dans un premier temps on veut afficher un input text et un bouton valider.
-Lorsque je rentre une tâche (contrôler que l'utilisateur ne rentre pas une chaîne de caractères vides)
-et que j'appuie sur "valider"), la tâche doit être ajoutée à la liste.
+Dans un premier temps on veut afficher un input text et un bouton valider. OK
+Lorsque je rentre une tâche (contrôler que l'utilisateur ne rentre pas une chaîne de caractères vide)
+et que l'on appuie sur "valider", la tâche doit être ajoutée à la liste.
 */
 
-/*
-Un autre commentaire histoire de vérifier que tout commit bien
-*/
 
 
 app.listen(8080);
