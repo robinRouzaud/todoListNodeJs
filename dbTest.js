fe2aaaa8e575d9f45e00441043276fa8218461ef
Robin ROUZAUD <rouzaud.robin@orange.fr>
2018-09-21 23:18:28 +0200

    Test file for sequelize setup: works fine

--- dbTest.js
+++ dbTest.js
@@ -4,7 +4,11 @@ et la récupération des données associées à l'objets
 Illustration sur l'utilisation des Promises et de la nécessité d'utiliser ".then()"
 */
 
-var dao = require('./dao');
+//var dao = require('./dao');
+
+var User = require('./models').User;
+var TodoList = require('./models').TodoList;
+var Task = require('./models').Task;
 
 /*
 var user1 = dao.UserDAO.createUser('prenomDAO9', 'nomDAO9', 'emailDAO9@test.com', 'passwordDAO9')
@@ -88,9 +92,52 @@ dao.UserDAO.findUserByEmail("emailDAO101@test.com")
 
 //dao.TaskListDAO.createTaskList('essai dbTest', 'ead50a80-ac7d-11e8-8ff9-a95ec1f48af4');
 
+/*
 dao.TaskDAO.createTask('Tache1');
 dao.TaskDAO.createTask('Tache2').then(task => {
     console.log(task);
 });
+*/
 
 
+User.create({
+    firstName: "Alex",
+    lastName: "Misovic",
+    email: "",
+    password: "poutchi"
+}).then(user => {
+    user.createTodoList({
+        name: "Ma première liste"
+    }).then(list => {
+        list.createTask({
+            name: "Jouer à mon iPad.",
+            quantity: 1
+        });
+        list.createTask({
+            name: "Regarder le shopping",
+            quantity: 9999
+        });
+        list.createTask({
+            name: "Caresser Greschka",
+            quantity: 999999999
+        });
+    });
+    user.createTodoList({
+        name: "J'adore les listes"
+    }).then(list => {
+        list.createTask({
+            name: "Jouer à mon iPad. Encore.",
+            quantity: 1
+        });
+        list.createTask({
+            name: "Faire une liste de shopping",
+            quantity: 9999
+        });
+        list.createTask({
+            name: "Caresser Greschka",
+            quantity: 999999999
+        });
+    });
+}).then(
+    console.log("Tout est enregistré!")
+);
